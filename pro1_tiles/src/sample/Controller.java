37db422a900bd097d76f8fad49d4b883bd1928af
Dawid Kotlarz <50261255+dawid0913@users.noreply.github.com>
2020-01-21 21:18:37 +0100

    kafelki i zdjęcia

--- pro1_tiles/src/sample/Controller.java
+++ pro1_tiles/src/sample/Controller.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+import java.util.concurrent.TimeUnit;
+
 public class Controller {
 
     @FXML
@@ -15,6 +17,10 @@ public class Controller {
     @FXML
     private Button tile15, tile16, tile17, tile18, tile19;
 
+    /**
+     * "Mapa" kafelków - tablica przechowująca układ zdjęć
+     * Tablica ma być losowan na serwerze i przesyłana do klientów.
+     */
     private String[] tilesMap = {
         "img1", "img2", "img3", "img4", "img5",
         "img7", "img10", "img6", "img2", "img8",
@@ -22,14 +28,34 @@ public class Controller {
         "img9", "img5", "img1", "img8", "img6",
     };
 
+    /**
+     * Tablica przechowuje nazwy klas klikniętych zdjęć
+     */
+    private String[] showImgArr = new String[2];
 
-    private String[] showImgArr;
-    //private Button[] showButtonArr;
-    private Integer countShowTiles;
+    /**
+     * Tablica przechowuje kliknięte buttony
+     */
+    private Button[] showButtonArr = new Button[2];
+
+    /**
+     * Zmienna zliczająca kliknięcia kafelków - liczy do 2
+     */
+    private Integer countClickedTiles = 0;
+
+    /**
+     * Tablica przechowuje id usuniętych kaflków-buttonów tzn. już poprawnie odkryte czyli dopasowane ze sobą
+     */
+    private String[] removeButtonArr = new String[20];
+
+    /**
+     * Zmienna zliczjąca usunięte kafelki
+     */
+    private Integer countRemoveButton = 0;
+
 
 
     public Controller() {
-        countShowTiles = 1;
         System.out.println("Controler działa");
     }
 
@@ -38,7 +64,7 @@ public class Controller {
     void initialize() {
         //img1 = "-fx-background-image: url('https://ocdn.eu/pulscms-transforms/1/c2iktkpTURBXy8wNWIxNDFiZmE2ZGNkYmExOGNkMWNjNmMxYzQ5ZTNhMS5qcGeRkwIAzQHk')";
         //img2 = "-fx-background-image: url('https://i.pinimg.com/originals/81/ef/53/81ef53720cd4342e057b99a012fd9a1c.jpg')";
-        //img2 = "-fx-background-image: url('./sample/img/nosacz.jpg')"; // lokalny plik nie dziala
+        //img2 = "-fx-background-image: url('img/nosacz.jpg')"; // lokalny plik nie dziala
         //tile1.getStyleClass().add("tileBlank");
         //tile5.setStyle(img1);
 
@@ -46,7 +72,7 @@ public class Controller {
 
 
     @FXML
-    public void tileClick(ActionEvent event){
+    public void tileClick(ActionEvent event) {
         Button clickedButton = (Button) event.getSource();
         //clickedButton.setStyle(img1);
         //clickedButton.getStyleClass().add("img1");
@@ -59,26 +85,61 @@ public class Controller {
      * Pokazanie kafelka
      * @param clickedButton
      */
-    public void showTiles(Button clickedButton){
-        
-        showImg(clickedButton, countShowTiles); //to do zahaszowania bo w ifie powinno działać a nie działa, czemu?
+    public void showTiles(Button clickedButton) {
 
-        /*if(countShowTiles < 2){
+        //showImg(clickedButton, countClickedTiles);
 
-            showButtonArr[countShowTiles] = clickedButton;
-            showImg(clickedButton, countShowTiles);
-            countShowTiles++;
-            if(countShowTiles == 2){
+        if(countClickedTiles < 2){
+
+            showButtonArr[countClickedTiles] = clickedButton;
+            showImg(clickedButton, countClickedTiles);
+            countClickedTiles++;
+            if(countClickedTiles == 2){
+                /*try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    System.out.println("błąd");
+                    return;
+                }*/
                 //checkShowTiles();
             }
 
-        }*/
+        }
     }
 
+
+    /**
+     * Sprawdza czy odkryte kafelki są takie same
+     */
 
     public void checkShowTiles(){
-
+        if(showImgArr[0] == showImgArr[1]){
+            System.out.println("nie działa");
+            ///removeTiles();
+        } else {
+            hideTiles();    //ukrywam kafelki
+            countClickedTiles = 0;  //ustawiam countClickedTiles na 0 aby móc dalej odkrywać kafelki
+            //ale tu trzeba też wywołać jakąś metodę blokującą tego klienta
+            //i wysyłającą informację do serwera aby odblokował drugiego gracza
+        }
     }
+
+
+    public void hideTiles(){
+        for(Integer i = 0; i<2; i++){
+            showButtonArr[i].getStyleClass().remove(showImgArr[i]);
+        }
+    }
+/*
+    public void removeTiles(){
+        for(Integer j = 0; j<2; j++){
+            removeButtonArr[countRemoveButton] = showButtonArr[j].getId();
+            countRemoveButton++;
+            showButtonArr[j].getStyleClass().remove(showImgArr[j]);
+            showButtonArr[j].getStyleClass().add("tileRemove");
+    }
+*/
+
 
 
     /**
